5b3e72fe52f5bac610b776f1a0515b342f1c2bc2
hijakd <jharyd@gmail.com>
2025-06-26 20:07:30 +1000

    Cactus Notes init commit

---
+++ app/src/main/java/com/hijakd/cactusnotes/ui/theme/Color.kt
@@ -0,0 +1,45 @@
+package com.hijakd.cactusnotes.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val PurpleGrey = Color(0xFF564E7A)
+
+val LightPurple = Color(0xFFE9D7F7)
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+
+val Grey = Color(0xFF808080)
+val LightGrey = Color(0xFFC0C0C0)
+
+val Pink = Color(0xFFE91E63)
+val Red = Color(0xFFFF0000)
+
+val Orange = Color(0xFFFF5722)
+val Yellow = Color(0xFFFFFF00)
+
+val Green80 = Color(0xFFC1F3C1)
+val NeonGreen = Color(0xFF00FF00)
+val MathGreen = Color(0xFF028074)
+val Teal = Color(0xFF008080)
+
+val IceBlue = Color(0xFF63B5F6)
+val MathBlue = Color(0xFF0479D6)
+val Blue = Color(0xFF0000FF)
+val OnSurface = Color(0xFF1C1B1F)
+
+val NoPlaceLikeHome = Color(0x037F0000)
+
+val Background = Color(0xFFC5A7D2)
