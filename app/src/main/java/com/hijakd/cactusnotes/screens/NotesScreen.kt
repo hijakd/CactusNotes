2966cd3fb4409ffcaae1a1e38e758926cd54f061
hijakd <jharyd@gmail.com>
2025-06-27 00:32:46 +1000

    added txt input for notes

--- app/src/main/java/com/hijakd/cactusnotes/screens/NotesScreen.kt
+++ app/src/main/java/com/hijakd/cactusnotes/screens/NotesScreen.kt
@@ -1,11 +1,16 @@
 package com.hijakd.cactusnotes.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -14,10 +19,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.hijakd.cactusnotes.components.NoteInputText
 import com.hijakd.cactusnotes.data.DummyNotes
 import com.hijakd.cactusnotes.model.Note
 import com.hijakd.cactusnotes.ui.theme.NeonGreen
@@ -31,15 +38,53 @@ fun NotesScreen(modifier: Modifier = Modifier, notes: List<Note>, onRemoveNote: 
     val ctx = LocalContext.current
 
     Column(modifier.fillMaxSize()) {
-        TopAppBar(title = { Text("Notes") }, colors = TopAppBarDefaults.topAppBarColors(NeonGreen)) // END of TopAppBar
+        TopAppBar(
+            title = { Text("Notes") },
 
-        Column(modifier.fillMaxWidth()) {
-            val txtModifier = modifier.padding(top = 9.dp, bottom = 8.dp)
+            actions = {
+                Icon(
+                    imageVector = Icons.Rounded.Add,
+                    contentDescription = "add note",
+                    modifier
+                            .padding(end = 10.dp)
+                            .size(42.dp)
+                )
+            },
+            colors = TopAppBarDefaults.topAppBarColors(NeonGreen)
+        ) // END of TopAppBar
 
-            Text("placeholder for note title input")
-            Text("placeholder for note body input")
+        Column(modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            val txtModifier = modifier
+                    .padding(top = 6.dp, bottom = 7.dp)
+                    .fillMaxWidth(0.9f)
 
-            HorizontalDivider(modifier.padding(horizontal = 10.dp, vertical = 15.dp))
+            Spacer(modifier = Modifier.size(7.dp))
+
+            NoteInputText(
+                modifier = txtModifier,
+                text = title,
+                label = "Title",
+                onTextChange = {
+                    if (it.all { char ->
+                            char.isLetter() || char.isWhitespace() || char.isDigit()
+                        }) title = it
+                }
+            )
+
+            NoteInputText(
+                modifier = txtModifier,
+                text = body,
+                label = "Body",
+                maxLine = 10,
+                onTextChange = {
+                    if (it.all { char ->
+                            char.isLetter() || char.isWhitespace() || char.isDigit()
+                        }) body = it
+                }
+            )
+
+
+            HorizontalDivider(modifier.padding(horizontal = 10.dp, vertical = 10.dp))
 
         } // END of "content" Column
 
