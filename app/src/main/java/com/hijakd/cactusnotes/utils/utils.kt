f34d4c32de69d980d5aedc6593fe9e4031b203c2
hijakd <jharyd@gmail.com>
2025-06-30 11:48:45 +1000

    move DataConverter to separate file

--- app/src/main/java/com/hijakd/cactusnotes/utils/utils.kt
+++ app/src/main/java/com/hijakd/cactusnotes/utils/utils.kt
@@ -1,6 +1,5 @@
 package com.hijakd.cactusnotes.utils
 
-import androidx.room.TypeConverter
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -15,16 +14,4 @@ fun formatDate(time: Date): String{
 //    val date = Date(time)
     val format = SimpleDateFormat("EEE. d MMM y - HH:mm", Locale.getDefault())
     return format.format(time)
-}
-
-class DateConverter {
-    @TypeConverter
-    fun dateToTimestamp(date: Date): Long{
-        return date.time
-    }
-
-    @TypeConverter
-    fun timestampToDate(timestamp: Long): Date?{
-        return Date(timestamp)
-    }
 }
